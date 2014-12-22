08a6ff3755398a6ad91932fb0e19805217b27b60
Patrick Steadman <ptsteadman@gmail.com>
2014-12-22 15:06:30 -0500

    new stuff

--- stories/chelsea/narrative_tree.js
+++ stories/chelsea/narrative_tree.js
@@ -1,10 +1,28 @@
-document.addEventListener('DOMContentLoaded', function(){
-    console.log("hi");
-    var rt = document.createElement('div');
-    rt.class = 'text-node';
-    rt.innerHTML = root['text'];
-    var text = document.querySelector('#text');
-    console.log(document.body);
-    document.body.appendChild(rt);
-});
+$(document).ready(function(){
 
+    $(".narrative_tree>.node").show();
+    
+    var liveText = function(nid){
+        console.log(nid);
+        var text = root.find(".node[data-nid=\"" + nid + "\"]>.text").html();
+        var liveText = $("<div class='live_text'>").html(text);
+        liveText.attr('data-nid', nid).hide();
+        return liveText;
+    }
+
+    var showLiveText = function(nid){
+        $(".live_text[data-nid=\"" + nid + "\"]").fadeIn();
+    }
+
+    var root = $(".narrative_tree").clone();
+    $(".narrative").html(liveText('root').show());
+    $(".narrative").on("click",".link", function(){
+        var nid = $(this).attr('data-nid');
+        var newText = liveText(nid);
+        $(".replace[data-nid=\"" + nid + "\"]").fadeOut(500, function(){
+            $(".replace[data-nid=\"" + nid + "\"]").replaceWith(newText);
+            showLiveText(nid);
+        });
+    });
+
+});
