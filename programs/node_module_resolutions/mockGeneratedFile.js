622f50d1f4ce211d7a526304315a0b462df4785a
Patrick Steadman <ptsteadman@gmail.com>
2024-12-04 20:59:28 -0500

    Add new highlights
    Signed-off-by: Patrick Steadman <ptsteadman@gmail.com>

---
+++ programs/node_module_resolutions/mockGeneratedFile.js
@@ -0,0 +1,6 @@
+const jspb = require('google-protobuf')
+
+const foo = { bar: 'baz' };
+// console.log(jspb)
+
+module.exports = foo;
---
+++ programs/node_module_resolutions/project/index.js
@@ -0,0 +1,5 @@
+const foo = require('../mockGeneratedFile.js')
+// const jspb = require('google-protobuf')
+
+
+console.log(`Hello ${foo.bar}!`);
